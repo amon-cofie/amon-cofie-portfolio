d72dfe7312b48c1b3533f29b6c008ee47ed5ad54
David  Amon-Cofie <david@goyazi.com>
2023-03-24 12:18:02 +0000

    Add pop up window

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// toggle icon
+/* toggle icon */
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
   navbar.classList.toggle('active');
 };
 
-// Active navlink styled
+/* Active navlink styled */
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -28,11 +28,11 @@ window.onscroll = () => {
     }
   });
 
-  // Make header sticky
+  /* Make header sticky */
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 100);
 
-  // Remove toggle
+  /* Remove toggle */
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
@@ -63,4 +63,139 @@ const typed = new Typed('.multiple-text', {
   backSpeed: 10,
   backDelay: 1000,
   loop: true,
+});
+
+/* The pop up menu */
+const popUpMenu = (project) => {
+  console.log(project);
+
+  const popUpContainer = document.createElement('div');
+  popUpContainer.classList.add('popup-container');
+
+  const popUpImage = document.createElement('img');
+  popUpImage.src = project.image;
+
+  const popUpDetailsContainer = document.createElement('div');
+  popUpDetailsContainer.classList.add('popup-details-container');
+
+  const popUpTitle = document.createElement('h2');
+  popUpTitle.classList.add('popup-title-container');
+  popUpTitle.textContent = project.title;
+
+  const popUpDetails = document.createElement('p');
+  popUpDetails.classList.add('popup-details-container');
+  popUpDetails.textContent = project.details;
+
+  const popUpBtnsContainer = document.createElement('p');
+  popUpBtnsContainer.classList.add('popup-btns-container');
+
+  const popGithubBtn = document.createElement('i');
+  popGithubBtn.classList.add('bx', 'bxl-github');
+
+  const closeBtn = document.createElement('i');
+  closeBtn.classList.add('bx', 'bx-x', 'close-btn');
+
+  const githubBtn = document.createElement('a');
+  githubBtn.appendChild(popGithubBtn);
+  githubBtn.classList.add('btn', 'github-btn');
+
+  const seeLiveBtn = document.createElement('a');
+  seeLiveBtn.textContent = 'See Live';
+  seeLiveBtn.classList.add('btn', 'see-live-btn');
+
+  popUpBtnsContainer.appendChild(githubBtn);
+  popUpBtnsContainer.appendChild(seeLiveBtn);
+
+  popUpDetailsContainer.appendChild(closeBtn);
+  popUpDetailsContainer.appendChild(popUpTitle);
+  popUpDetailsContainer.appendChild(popUpDetails);
+  popUpDetailsContainer.appendChild(popUpBtnsContainer);
+
+  popUpContainer.appendChild(popUpImage);
+  popUpContainer.appendChild(popUpDetailsContainer);
+  document.body.appendChild(popUpContainer);
+
+  closeBtn.addEventListener('click', () => {
+    document.body.removeChild(popUpContainer);
+  });
+};
+
+/* Render projects dynamically */
+const projectsArr = [
+  {
+    image: 'https://picsum.photos/300/200',
+    title: 'Web Design',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+  },
+  {
+    image: 'https://picsum.photos/300/200',
+    title: 'Web Design',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+  },
+  {
+    image: 'https://picsum.photos/300/200',
+    title: 'Web Design',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+  },
+  {
+    image: 'https://picsum.photos/300/200',
+    title: 'Web Design',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+  },
+  {
+    image: 'https://picsum.photos/300/200',
+    title: 'Web Design',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+  },
+  {
+    image: 'https://picsum.photos/300/200',
+    title: 'Web Design',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+  },
+];
+
+const projectsContainer = document.querySelector('.portfolio-container');
+
+projectsArr.forEach((proj) => {
+  const project = document.createElement('div');
+  project.classList.add('portfolio-box');
+
+  const projectImg = document.createElement('img');
+  projectImg.src = proj.image;
+  projectImg.alt = proj.title;
+
+  const layer = document.createElement('div');
+  layer.classList.add('portfolio-layer');
+
+  const title = document.createElement('h4');
+  title.textContent = proj.title;
+
+  const details = document.createElement('p');
+  details.textContent = proj.details;
+
+  const popLink = document.createElement('a');
+
+  const popBtn = document.createElement('i');
+  popBtn.classList.add('bx', 'bx-link-external');
+
+  popLink.appendChild(popBtn);
+
+  layer.appendChild(title);
+  layer.appendChild(details);
+  layer.appendChild(popLink);
+
+  project.appendChild(projectImg);
+  project.appendChild(layer);
+
+  projectsContainer.appendChild(project);
+
+  popLink.addEventListener('click', () => {
+    popUpMenu(proj);
+  });
 });
