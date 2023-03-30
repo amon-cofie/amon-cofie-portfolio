31ef1912290fc6a0887c0cbde07c0a114f7b09c0
David  Amon-Cofie <david@goyazi.com>
2023-03-30 11:07:01 +0000

    Populate projects with projects and add popup menu

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.onscroll = () => {
 };
 
 ScrollReveal({
-  //   reset: true,
+  // reset: true,
   distance: '80px',
   duration: '2000',
   delay: '200',
@@ -65,98 +65,119 @@ const typed = new Typed('.multiple-text', {
   loop: true,
 });
 
+
+
 /* The pop up menu */
 const popUpMenu = (project) => {
-  console.log(project);
-
-  const popUpContainer = document.createElement('div');
-  popUpContainer.classList.add('popup-container');
+  const popUpModal = document.querySelector('#modal');
+  const popUpModalOverlay = document.querySelector('#overlay');
+  const popUpModalCloseBtn = document.querySelector('[data-close-button]');
 
   const popUpImage = document.createElement('img');
   popUpImage.src = project.image;
+  popUpImage.classList.add('modal-image');
 
-  const popUpDetailsContainer = document.createElement('div');
-  popUpDetailsContainer.classList.add('popup-details-container');
+  const popUpDetails = document.createElement('div');
+  popUpDetails.classList.add('modal-details');
+  popUpDetails.id = 'modal-details';
 
-  const popUpTitle = document.createElement('h2');
-  popUpTitle.classList.add('popup-title-container');
+  const popUpTitle = document.createElement('h3');
+  popUpTitle.classList.add('modal-header');
   popUpTitle.textContent = project.title;
+  popUpDetails.appendChild(popUpTitle);
 
-  const popUpDetails = document.createElement('p');
-  popUpDetails.classList.add('popup-details-container');
-  popUpDetails.textContent = project.details;
+  const popUpText = document.createElement('p');
+  popUpText.classList.add('project-details');
+  popUpText.textContent = project.details;
+  popUpDetails.appendChild(popUpText);
 
-  const popUpBtnsContainer = document.createElement('p');
+  const repoBtn = document.createElement('a');
+  const seeLive = document.createElement('a');
+  repoBtn.classList.add('btn', 'project-btns');
+  seeLive.classList.add('btn', 'project-btns');
+  repoBtn.textContent = 'GitHub';
+  repoBtn.href = project.githubLink;
+  seeLive.textContent = 'Live';
+  seeLive.href = project.liveLink;
+
+  const popUpBtnsContainer = document.createElement('div');
   popUpBtnsContainer.classList.add('popup-btns-container');
 
-  const popGithubBtn = document.createElement('i');
-  popGithubBtn.classList.add('bx', 'bxl-github');
+  popUpBtnsContainer.appendChild(repoBtn);
+  popUpBtnsContainer.appendChild(seeLive);
 
-  const closeBtn = document.createElement('i');
-  closeBtn.classList.add('bx', 'bx-x', 'close-btn');
+  popUpDetails.appendChild(popUpBtnsContainer);
 
-  const githubBtn = document.createElement('a');
-  githubBtn.appendChild(popGithubBtn);
-  githubBtn.classList.add('btn', 'github-btn');
+  popUpModal.appendChild(popUpImage);
+  popUpModal.appendChild(popUpDetails);
 
-  const seeLiveBtn = document.createElement('a');
-  seeLiveBtn.textContent = 'See Live';
-  seeLiveBtn.classList.add('btn', 'see-live-btn');
+  popUpModal.classList.add('active');
+  popUpModalOverlay.classList.add('active');
 
-  popUpBtnsContainer.appendChild(githubBtn);
-  popUpBtnsContainer.appendChild(seeLiveBtn);
+  popUpModalCloseBtn.addEventListener('click', () => {
+    popUpModal.classList.remove('active');
+    popUpModalOverlay.classList.remove('active');
+    popUpModal.removeChild(popUpImage);
+    popUpModal.removeChild(popUpDetails);
+  });
 
-  popUpDetailsContainer.appendChild(closeBtn);
-  popUpDetailsContainer.appendChild(popUpTitle);
-  popUpDetailsContainer.appendChild(popUpDetails);
-  popUpDetailsContainer.appendChild(popUpBtnsContainer);
-
-  popUpContainer.appendChild(popUpImage);
-  popUpContainer.appendChild(popUpDetailsContainer);
-  document.body.appendChild(popUpContainer);
-
-  closeBtn.addEventListener('click', () => {
-    document.body.removeChild(popUpContainer);
+  popUpModalOverlay.addEventListener('click', () => {
+    popUpModal.classList.remove('active');
+    popUpModalOverlay.classList.remove('active');
+    popUpModal.removeChild(popUpImage);
+    popUpModal.removeChild(popUpDetails);
   });
 };
 
 /* Render projects dynamically */
 const projectsArr = [
   {
-    image: 'https://picsum.photos/300/200',
-    title: 'Web Design',
+    image: 'images/project_screen_shots/awesome_books_2.png',
+    title: 'Awesome Books',
+    githubLink: 'https://github.com/amon-cofie/Awesome-books-ES6',
+    liveLink: 'https://amon-cofie.github.io/Awesome-books-ES6/',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+      'A Single Page Application featuring a medium fidelity wireframe of a book management app that stores books that are added and which can be deleted. Built mainly with JavaScript and strict ES6 features.',
   },
   {
-    image: 'https://picsum.photos/300/200',
-    title: 'Web Design',
+    image: 'images/project_screen_shots/spend_right_2.png',
+    title: 'Spend Right',
+    githubLink: 'https://github.com/amon-cofie/spend-right-budget-app',
+    liveLink: 'https://spend-right.onrender.com/',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+      'Spend Right is a full stack ruby on rails web application on a PostgreSQL database that is built to manage your finances by grouping your expenses or transactions into categories and helping you visualize how much you are spending and where you are spending the most amount of money.',
   },
   {
-    image: 'https://picsum.photos/300/200',
-    title: 'Web Design',
+    image: 'images/project_screen_shots/to_do_list_2.png',
+    title: 'To-Do-List',
+    githubLink: 'https://github.com/amon-cofie/To-Do-List',
+    liveLink: 'https://amon-cofie.github.io/To-Do-List/',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+      'A Single Page Application featuring a medium fidelity wireframe of a to-do-list app that stores tasks that are added and which can be deleted. The tasks can be checked, unchecked or edited. Built mainly with JavaScript and strict ES6 features. Bundled with webpack.',
   },
   {
-    image: 'https://picsum.photos/300/200',
-    title: 'Web Design',
+    image: 'images/project_screen_shots/bmen_conference_2.png',
+    title: 'BMEN 2022',
+    githubLink: 'https://github.com/amon-cofie/first-capstone-project',
+    liveLink: 'https://amon-cofie.github.io/first-capstone-project/',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+      `My first capstone project in microverse, a two webpage site for a pretend biomedical engineering conference. Inspiration for this project was obtained from Cindy Shin's behance design of a website for a Creative Commons Summit set to happen in October 2022. Built with HTML, CSS and JavaScript.`,
   },
   {
-    image: 'https://picsum.photos/300/200',
-    title: 'Web Design',
+    image: 'images/project_screen_shots/air_pollution_tracker_1.png',
+    title: 'AQI Tracker',
+    githubLink: 'https://github.com/amon-cofie/Air-Pollution-Tracking-App',
+    liveLink: 'https://airpollutiontracker.netlify.app/',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+      'This is a simple single page application that tracks current air pollution data in any city in the world. It gives hourly average data on levels of concentration of various pollutes including Carbon Mono-Oxide, Ozone, PM25, PM10 among others. It is a good place to check the quality of the air in your local city or any city you would like to visit.',
   },
   {
-    image: 'https://picsum.photos/300/200',
-    title: 'Web Design',
+    image: 'images/project_screen_shots/leaderboard_2.png',
+    title: 'Leaderboard',
+    githubLink: 'https://github.com/amon-cofie/leaderboard-project',
+    liveLink: 'https://amon-cofie.github.io/leaderboard-project/dist/',
     details:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut , voluptas?',
+      'A JavaScript web app that contains the high scores of your games and the players that had those scores. It lists the scores from the highest to the lowest, showing the player with the highest scores. Bundled with webpack.',
   },
 ];
 
@@ -196,6 +217,7 @@ projectsArr.forEach((proj) => {
   projectsContainer.appendChild(project);
 
   popLink.addEventListener('click', () => {
+    console.log('Has been clicked');
     popUpMenu(proj);
   });
 });
