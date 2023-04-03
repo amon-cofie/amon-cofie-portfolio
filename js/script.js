f845952043a80b4e0400e9fa9c9e03f4257ba34c
David  Amon-Cofie <david@goyazi.com>
2023-04-03 11:10:58 +0000

    Add target attribute to links to open in new tabs

--- js/script.js
+++ js/script.js
@@ -137,7 +137,7 @@ const projectsArr = [
     githubLink: 'https://github.com/amon-cofie/Awesome-books-ES6',
     liveLink: 'https://amon-cofie.github.io/Awesome-books-ES6/',
     details:
-      'A Single Page Application featuring a medium fidelity wireframe of a book management app that stores books that are added and which can be deleted. Built mainly with JavaScript and strict ES6 features.',
+      'This project showcases a Single Page Application featuring a medium-fidelity wireframe of a book management app that stores books that can be added and deleted. It was developed primarily using JavaScript and strict ES6 features.',
   },
   {
     image: 'images/project_screen_shots/spend_right_2.png',
@@ -145,7 +145,7 @@ const projectsArr = [
     githubLink: 'https://github.com/amon-cofie/spend-right-budget-app',
     liveLink: 'https://spend-right.onrender.com/',
     details:
-      'Spend Right is a full stack ruby on rails web application on a PostgreSQL database that is built to manage your finances by grouping your expenses or transactions into categories and helping you visualize how much you are spending and where you are spending the most amount of money.',
+      'Spend Right is a full stack ruby on rails web application on a PostgreSQL database, built to manage your finances by grouping your expenses or transactions into categories and helping you visualize how much you are spending and where you are spending the most amount of money.',
   },
   {
     image: 'images/project_screen_shots/to_do_list_2.png',
@@ -153,15 +153,14 @@ const projectsArr = [
     githubLink: 'https://github.com/amon-cofie/To-Do-List',
     liveLink: 'https://amon-cofie.github.io/To-Do-List/',
     details:
-      'A Single Page Application featuring a medium fidelity wireframe of a to-do-list app that stores tasks that are added and which can be deleted. The tasks can be checked, unchecked or edited. Built mainly with JavaScript and strict ES6 features. Bundled with webpack.',
+      'This project features a Single Page Application featuring a medium fidelity wireframe of a to-do-list app that stores tasks that a user adds. These tasks can be marked as completed or incomplete. They can also be edited or deleted. This project is built mainly with JavaScript and strict ES6 features and bundled with Webpack.',
   },
   {
     image: 'images/project_screen_shots/bmen_conference_2.png',
     title: 'BMEN 2022',
     githubLink: 'https://github.com/amon-cofie/first-capstone-project',
     liveLink: 'https://amon-cofie.github.io/first-capstone-project/',
-    details:
-      'My first capstone project in microverse, a two webpage site for a pretend biomedical engineering conference. Inspiration for this project was obtained from Cindy Shin\'s behance design of a website for a Creative Commons Summit set to happen in October 2022. Built with HTML, CSS and JavaScript.',
+    details: `This project features a two-page website for a pretend biomedical engineering conference. Inspiration for this project was obtained from Cindy Shin's Behance design of a website for a Creative Commons Summit set to happen in October 2022. It is built with HTML, CSS, and JavaScript.`,
   },
   {
     image: 'images/project_screen_shots/air_pollution_tracker_1.png',
@@ -169,7 +168,7 @@ const projectsArr = [
     githubLink: 'https://github.com/amon-cofie/Air-Pollution-Tracking-App',
     liveLink: 'https://airpollutiontracker.netlify.app/',
     details:
-      'This is a simple single page application that tracks current air pollution data in any city in the world. It gives hourly average data on levels of concentration of various pollutes including Carbon Mono-Oxide, Ozone, PM25, PM10 among others. It is a good place to check the quality of the air in your local city or any city you would like to visit.',
+      'This project showcases a simple Single Page Application that tracks current air pollution data in any city in the world. It gives hourly-average data on levels of concentration of various pollutes including Carbon Mono-Oxide, Ozone, PM25, and PM10. It is useful for checking the air quality in your local city or a city you would like to visit.',
   },
   {
     image: 'images/project_screen_shots/leaderboard_2.png',
@@ -177,7 +176,7 @@ const projectsArr = [
     githubLink: 'https://github.com/amon-cofie/leaderboard-project',
     liveLink: 'https://amon-cofie.github.io/leaderboard-project/dist/',
     details:
-      'A JavaScript web app that contains the high scores of your games and the players that had those scores. It lists the scores from the highest to the lowest, showing the player with the highest scores. Bundled with webpack.',
+      'This project showcases a JavaScript web app that stores the high scores for your games and the players that had those scores. It lists the scores in descending order, revealing the player with the highest score. It is bundled with Webpack.',
   },
 ];
 
